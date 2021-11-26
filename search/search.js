b0ccf5a149de4411eb6b68c6c5d2b743a61b6e54
Timothy Limpin <Timothy_Limpin@hotmail.ca>
2021-11-25 21:00:32 -0500

    search update

--- search/search.js
+++ search/search.js
@@ -1,34 +1,48 @@
-function geocode(){
+function geocode() {
     var location = document.getElementById('ipinput').value;
-    axios.get('https://api.ipfind.com/',{
-        params:{
+    axios.get('https://api.ipfind.com/', {
+        params: {
             ip: location,
-            auth:'efb0771c-9a9f-4a3f-808a-8dfaa1ee937d'
+            auth: 'efb0771c-9a9f-4a3f-808a-8dfaa1ee937d'
         }
     })
-    .then(function(response){
-        console.log(response)
+        .then(function (response) {
+            console.log(response)
 
-        //formatted address
-        var lat = response.data.latitude;
-        var latOutput = `
+            //formatted address
+            var lat = response.data.latitude;
+            var latOutput = `
             <ul class = "list-group">
                 <li class = "list-group-item">${lat}</li>
             </ul>
         `;
-        var long = response.data.longitude;
-        var longOutput = `
-            <ul class = "list-group">
-                <li class = "list-group-item">${long}</li>
-            </ul>
-        `;
-        
-        //output to app
-        document.getElementById('formattedaddlat').innerHTML=latOutput
-        document.getElementById('formattedaddlong').innerHTML=longOutput
-        
-    })
-    .catch(function(error){
-        console.log(error);
-    });
+            var long = response.data.longitude;
+             var longOutput = `
+             <ul class = "list-group">
+                 <li class = "list-group-item">${long}</li>
+             </ul>
+         `;
+
+            //output to app
+            document.getElementById('formattedaddlat').innerHTML = latOutput
+            document.getElementById('formattedaddlong').innerHTML = longOutput
+            var map = L.map('map').setView([latOutput, longOutput], 13);
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmVudGlpY2Vkd2F0ZXIiLCJhIjoiY2t3ZnFkY2V2MGdydDJvbXB5NWF0dXZtaSJ9.9HwuYTrQREI1wmTfShkFyw', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'your.mapbox.access.token'
+            }).addTo(mymap);
+
+            L.marker([latOutput, longOutput]).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
