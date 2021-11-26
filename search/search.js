60c0bbe6d71ce19b7c1a469ea49ae6a1259a740b
Timothy Limpin <Timothy_Limpin@hotmail.ca>
2021-11-25 21:54:52 -0500

    final ipgeolocator

--- search/search.js
+++ search/search.js
@@ -17,28 +17,23 @@ function geocode() {
             </ul>
         `;
             var long = response.data.longitude;
-             var longOutput = `
+            var longOutput = `
              <ul class = "list-group">
                  <li class = "list-group-item">${long}</li>
              </ul>
          `;
 
             //output to app
-            document.getElementById('formattedaddlat').innerHTML = latOutput
-            document.getElementById('formattedaddlong').innerHTML = longOutput
-            var map = L.map('map').setView([latOutput, longOutput], 13);
+            //document.getElementById('formattedaddlat').innerHTML = latOutput
+            //document.getElementById('formattedaddlong').innerHTML = longOutput
+            var map = L.map('map').setView([lat, long], 13);
 
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmVudGlpY2Vkd2F0ZXIiLCJhIjoiY2t3ZnFkY2V2MGdydDJvbXB5NWF0dXZtaSJ9.9HwuYTrQREI1wmTfShkFyw', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox/streets-v11',
-                tileSize: 512,
-                zoomOffset: -1,
-                accessToken: 'your.mapbox.access.token'
-            }).addTo(mymap);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-            L.marker([latOutput, longOutput]).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            L.marker([lat, long]).addTo(map)
+                .bindPopup(lat+'<br>' + long)
                 .openPopup();
 
         })
